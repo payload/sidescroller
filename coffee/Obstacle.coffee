aa111aa23ac99dffa8e6c6d352a33ad2eae2e869
payload <payload@lavabit.com>
2011-03-27 20:34:27 +0200

    turned spaces into tabs

---
+++ coffee/Obstacle.coffee
@@ -0,0 +1,16 @@
+window.Obstacle = class Obstacle extends FlyingObject
+    constructor: (@world) ->
+        this.init(world)
+        
+        m = this.movement
+        dmg = this.damage
+
+        m.size.Set(10, 10)
+        m.vel.Set(-160 + 40 * Math.random(), 0)
+        m.vel_want.SetV(m.vel)
+
+        dmg.energy = 0
+        dmg.to_apply = 30
+        dmg.factor = 0
+        dmg.die = ->
+
--- js/main.js
+++ js/main.js
@@ -5,13 +5,6 @@ var include = function(file, path) {
 };
 var toinc = ["foo.js", "Game.js", "FlyingObject.js", "World.js",
 	"Units.js", "Sprites.js", "ShootingModel.js", "Shell.js",
-	"DamageModel.js", "MovementModel.js", "Timer.js"];
+	"DamageModel.js", "MovementModel.js", "Timer.js", "Obstacle.js"];
 for (var i in toinc) include(toinc[i]);
 include("b2Vec2.js", "js/");
-include("Obstacle.js", "js/");
-
-function inherit(a, b) {
-    a.prototype = new b();
-    a.prototype.constructor = a;
-    return a;
-}
